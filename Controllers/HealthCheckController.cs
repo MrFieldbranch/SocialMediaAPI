769276aa52fc4f17549882e470b689d85a78bcc6
Joel Åkergren <joel.akergren@gmail.com>
2025-02-26 10:38:03 +0100

    Add HealthCheckController

---
+++ Controllers/HealthCheckController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaAPI23Okt.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("API is running");
+    }
+}
--- obj/Debug/net8.0/SocialMediaAPI23Okt.AssemblyInfo.cs
+++ obj/Debug/net8.0/SocialMediaAPI23Okt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaAPI23Okt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+660e3de43c05ae135826661c1453f93875c291e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb424b5a228f446856cc7568e1d8ca252b1f179")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaAPI23Okt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaAPI23Okt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
